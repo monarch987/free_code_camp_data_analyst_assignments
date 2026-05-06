091b3f0d550f44e654eb37ea57bed94fefa67bba
monarch987 <akshaymonarch@gmail.com>
2026-05-06 15:00:35 +0530

    updated medical data visualizer

--- boilerplate-medical-data-visualizer-main/medical_data_visualizer.py
+++ boilerplate-medical-data-visualizer-main/medical_data_visualizer.py
@@ -4,57 +4,96 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
 df['overweight'] = None
 
 # 3
+def is_overweight(row):
+    
+    w = row['weight']
+    h = row['height']
+     
+    if h==0 or pd.isna(h) or pd.isna(w):
+        return 0
+
+    h = h/100 #since height s in cm converting it to meter
+    
+    bmi = (w/h**2)
+   
+    if bmi>25 : 
+        return 1 
+    elif bmi <=25: 
+        return 0
+    
+def normalize_values(x):
+    
+    if x <=1:
+        return 0
+    
+    elif x >1:
+        return 1
+    
+#inserting value along column hence axis=1
+df['overweight'] = df.apply(is_overweight,axis=1)
+
+### NORMALIZE GLUCOSE & CHOLESTROL VALUES so unfiormity is present in data to be rendered
+
+df['gluc'] = df['gluc'].apply(normalize_values)
+df['cholesterol'] = df['cholesterol'].apply(normalize_values)
 
 
-# 4
+# print(df_cat.head(10))
+# print(df.columns)
+
+#
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = df.melt(id_vars='cardio',value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'],var_name='variable')
 
 
-    # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio','variable','value']).size().reset_index(name='total')
+
+
+    fig = sns.catplot(data=df_cat,x='variable',y='total',hue='value',col="cardio",kind='bar')
     
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    fig.set_axis_labels('variable','total')
+    fig = fig.fig
     fig.savefig('catplot.png')
     return fig
 
 
 # 10
 def draw_heat_map():
+
     # 11
-    df_heat = None
+    df_heat = df[(df['ap_lo']<=df['ap_hi'])&
+    (df['height']>=df['height'].quantile(0.025))&
+    (df['height']<=df['height'].quantile(0.975))&
+    (df['weight']>=df['weight'].quantile(0.025))&
+    (df['weight']<=df['weight'].quantile(0.975))]
+
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr,dtype=bool))
 
     # 14
-    fig, ax = None
+    fig,ax = plt.subplots(figsize=(15,10)) 
+    sns.heatmap(corr,mask=mask,fmt='.1f',annot=True,ax=ax)
 
     # 15
-
-
 
     # 16
     fig.savefig('heatmap.png')
     return fig
+
+
+if __name__ == '__main__':
+
+    # draw_cat_plot()
+    # draw_heat_map()
+    pass
